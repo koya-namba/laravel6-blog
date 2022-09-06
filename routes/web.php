75c4144e8a713a94537e1c3f8ec2cd05f1b8a724
koya-namba <namba.koya@arakawa-lab.com>
2022-09-06 15:52:22 +0900

    create user.auth

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,18 @@
 |
 */
 
-Route::get('/', 'PostController@index');
-Route::post('/posts', 'PostController@store');
-Route::get('/posts/create', 'PostController@create');
-Route::get('/posts/{post}', 'PostController@show');
-Route::get('/posts/{post}/edit', 'PostController@edit');
-Route::put('/posts/{post}', 'PostController@update');
-Route::delete('/posts/{post}', 'PostController@delete');
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/', 'PostController@index');
+    Route::post('/posts', 'PostController@store');
+    Route::get('/posts/create', 'PostController@create');
+    Route::get('/posts/{post}', 'PostController@show');
+    Route::get('/posts/{post}/edit', 'PostController@edit');
+    Route::put('/posts/{post}', 'PostController@update');
+    Route::delete('/posts/{post}', 'PostController@delete');
+    Route::get('/categories/{category}', 'CategoryController@index');
+});
 
-Route::get('/categories/{category}', 'CategoryController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
